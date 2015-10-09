14bfad38fddb0e7de6ebdeed7e0c16dd09dbe8df
Sven Kreiss <me@svenkreiss.com>
2015-10-09 16:19:25 -0400

    specifiy supported Python versions

--- setup.py
+++ setup.py
@@ -58,5 +58,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ]
 )
