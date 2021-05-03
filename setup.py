c1207d23ae51ec57d2e9b6df82c9569dc9c492f1
Sven Kreiss <me@svenkreiss.com>
2021-05-03 08:20:21 +0200

    set long_description_content_type='text/x-rst'

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     license='MIT',
     description='Validate HTML5 files.',
     long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
     author='Sven Kreiss',
     author_email='me@svenkreiss.com',
     url='https://github.com/svenkreiss/html5validator',
